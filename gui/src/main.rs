6ef4d5d10c0edb852cead812208e313e4c5bbeef
Placeless <mikoprpr@hotmail.com>
2024-01-05 20:41:53 +0900

    nomsg

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -2,8 +2,10 @@
 mod main_copy;
 mod rotate;
 
+#[warn(unused_imports)]
 use crate::rotate::*;
 use crate::main_copy::hello;
+
 fn main() {
-    pub const fn hello(){};
+    pub const fn hello(){}
 }
