872d4b8b5790da9b10d012f96a8c751316704478
Placeless <mikoprpr@hotmail.com>
2024-01-08 23:38:02 +0900

    cargo fmt

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,7 +1,7 @@
 mod main_copy;
 use crate::main_copy::*;
 fn main() {
-    const fn main_copy(){
-       println!("Hello world!");
+    const fn main_copy() {
+        println!("Hello world!");
     }
 }
