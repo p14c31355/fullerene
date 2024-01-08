d1f81677213bb5ec25f13bb95c1b8629619fe046
Placeless <mikoprpr@hotmail.com>
2024-01-08 23:45:57 +0900

    compile tour5,7

--- gui/src/tour5.rs
+++ gui/src/tour5.rs
@@ -79,6 +79,47 @@ struct Foo<'a> {
     i:&'a i32
 }
 
+struct SeaCreature {
+    pub name: String,
+    noise: String,
+  }
+  trait LoudNoiseMaker: NoiseMaker {
+    fn make_alot_of_noise(&self) {
+        self.make_noise();
+        self.make_noise();
+        self.make_noise();
+    }
+    
+    fn dynamic_make_noise(noise_maker: &dyn NoiseMaker) {
+      // we don't know the real type
+      noise_maker.make_noise();
+    }
+    
+    fn generic_make_noise<T>(creature: &T)
+    where
+      T: NoiseMaker
+    {
+      // we know the real type at compile-time
+      creature.make_noise();
+    }
+  
+    fn make_noise(&self);
+    
+  }
+  
+  impl LoudNoiseMaker as NoiseMaker for SeaCreature {
+    fn make_noise(&self) {
+        println!("{}", &self.get_sound());
+    }
+  
+    pub fn get_sound(&self) -> &str {
+      &self.noise
+    }
+  }
+  struct Ocean {
+    animals: Vec<Box<dyn NoiseMaker>>,
+  }
+
 fn main() {
     // 構造体をインスタンス化し、変数に束縛してメモリリソースを作成
     let foo = Foo { x: 42 };
@@ -194,4 +235,30 @@ fn main() {
         i: &x
     };
     println!("{}",foo.i);
+
+    let ferris = SeaCreature {
+        name: String::from("Ferris"),
+        noise: String::from("blub"),
+      };
+    
+      let sarah = SeaCreature {
+        name: String::from("Sarah"),
+        noise: String::from("swish"),
+      };
+    
+      let ocean = Ocean {
+        animals: vec![Box::new(ferris), Box::new(sarah)],
+      };
+    
+      println!("{}", creature.get_sound());
+      creature.make_noise();
+      creature.make_alot_of_noise();
+      static_make_noise(&creature);
+      dynamic_make_noise(&creature);
+      generic_make_noise(&creature);
+    
+      for a in ocean.animals.iter() {
+        a.make_noise();
+    
+      };
 }
