8bd8ab75c50256ff73d38775cf68e0f2c2ff1293
Placeless <mikoprpr@hotmail.com>
2024-01-28 18:41:52 +0900

    ???

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,48 +1,55 @@
-// Cargo.tomlに依存関係を追加する
-// [dependencies]
-// avr-hal = "0.6"
-// avr-device = "0.7"
-// panic-halt = "0.2"
-// servo-sg90 = "0.1"
-
 #![no_std]
 #![no_main]
 
 use panic_halt as _;
 
-use avr_device::attiny85;
 use avr_hal::prelude::*;
-use avr_hal::clock::MHz8;
+use avr_hal::atmega328p;
+use avr_hal::port::mode::{Input, Output};
+use avr_hal::port::portb::PB1;
 use avr_hal::delay::Delay;
-use avr_hal::port::mode::Output;
 
-use servo_sg90::Servo;
+use avr_hal::pwm::Pwm1;
 
+// サーボモーターの角度範囲 (この範囲はサーボモーターによって異なります)
+const SERVO_MIN: u16 = 1000;
+const SERVO_MAX: u16 = 2000;
+
+// サーボモーターを動かす関数
+fn move_servo<T: Pwm1>(servo: &mut T, angle: u16) {
+    let duty_cycle = ((angle - SERVO_MIN) * (servo.get_max_duty() - servo.get_min_duty()))
+        / (SERVO_MAX - SERVO_MIN)
+        + servo.get_min_duty();
+    
+    servo.set_duty(duty_cycle);
+}
+
+// メイン関数
 #[avr_device::entry]
 fn main() -> ! {
-    // ATTiny85のピンの設定
-    let dp = attiny85::Peripherals::take().unwrap();
-    let mut portb = dp.PORTB.split();
+    let dp = atmega328p::Peripherals::take().unwrap();
+    let mut delay = Delay::new();
 
-    // サーボモーターの制御ピンの設定
-    let servo_pin = portb.pb0.into_output(&mut portb.ddr);
+    // PB1を出力モードに設定
+    let mut servo_pin: PB1<Output> = dp.PORTB.pb1.into_output(&dp.DDRB);
 
-    // サーボモーターの初期化
-    let mut servo = Servo::new(servo_pin);
-
-    let mut delay = Delay::<MHz8>::new();
+    // PWMを初期化
+    let mut pwm = dp.TC1.pwm(
+        servo_pin.into_output(&dp.DDRB),
+        avr_hal::pwm::Timer1Pwm::top::<avr_hal::time::Milliseconds>(20),
+    );
 
     loop {
-        // サーボを0度に動かす
-        servo.set_degrees(0);
+        // サーボモーターを0度に動かす
+        move_servo(&mut pwm, 1000);
         delay.delay_ms(1000);
 
-        // サーボを90度に動かす
-        servo.set_degrees(90);
+        // サーボモーターを90度に動かす
+        move_servo(&mut pwm, 1500);
         delay.delay_ms(1000);
 
-        // サーボを180度に動かす
-        servo.set_degrees(180);
+        // サーボモーターを180度に動かす
+        move_servo(&mut pwm, 2000);
         delay.delay_ms(1000);
     }
 }
