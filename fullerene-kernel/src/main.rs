7fcdf858c4ef30b3f8d2aa81cf9b0faa58c1fd3a
Placeless <thinkoverheat@gmail.com>
2025-09-19 10:54:25 +0900

    fix

--- fullerene-kernel/src/main.rs
+++ fullerene-kernel/src/main.rs
@@ -12,8 +12,8 @@ extern crate alloc;
 use core::ffi::c_void;
 use uefi::{EfiSystemTable, FULLERENE_FRAMEBUFFER_CONFIG_TABLE_GUID, FullereneFramebufferConfig};
 
-#[export_name = "efi_main"]
-#[link_section = ".text.efi_main"]
+#[unsafe(export_name = "efi_main")]
+#[unsafe(link_section = ".text.efi_main")]
 pub extern "efiapi" fn efi_main(
     _image_handle: usize,
     system_table: *mut c_void,
